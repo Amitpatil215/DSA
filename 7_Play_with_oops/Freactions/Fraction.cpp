979e60c4d19d8bcbac56d3c6a746bc82681a13cf
Amit Patil <amitgpatil215@gmail.com>
2021-04-18 14:26:57 +0530

    additions of fractions

---
+++ 7_Play_with_oops/Freactions/Fraction.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Fraction
+{
+private:
+    int num;
+    int den;
+
+public:
+    Fraction(int num, int den)
+    {
+        this->num = num;
+        this->den = den;
+    }
+
+    void print()
+    {
+        cout << num << "/" << den << endl;
+    }
+
+    void add(Fraction const &f2) // const &f2, reference pointer can only read data from f2
+                                 // but cannot change/write it
+    {
+        int lcm = den * f2.den;
+        int x = lcm / den;
+        int y = lcm / f2.den;
+        int num = x * this->num + y * f2.num;
+        this->num = num;
+        this->den = lcm;
+        simplify();
+    }
+
+    void simplify()
+    {
+        int gcd = 1;
+
+        ///! Manually find gcd
+        // int maxGcd = min(num, den);
+        // for (int i = 1; i <= maxGcd; i++)
+        // {
+        //     if (num % i == 0 && den % i == 0)
+        //     {
+        //         gcd = i;
+        //     }
+        // }
+
+        ///! find gcd using inbuild function in the algorithm library
+        gcd = __gcd(num, den);
+        num = num / gcd;
+        den = den / gcd;
+    }
+};
