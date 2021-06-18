f9b6ac0282b2114d112cb3691666efe4671e6e44
Amit Patil <amitgpatil215@gmail.com>
2021-06-18 11:50:53 +0530

    traverse using index for finding and element

--- 12_recursion_intermediate/3isElemetPresent.cpp
+++ 12_recursion_intermediate/3isElemetPresent.cpp
@@ -20,10 +20,28 @@ bool isPresent(int a[], int n, int x)
     return isFoundInSmallArray;
 }
 
+bool isPresent2(int a[], int n, int x, int i)
+{
+    //base case
+    if (i == n - 1)
+    {
+        return false;
+    }
+
+    if (a[i] == x)
+    {
+        return true;
+    }
+
+    bool isFoundInSmallArray = isPresent2(a, n, x, i + 1);
+
+    //calculation
+    return isFoundInSmallArray;
+}
 int main()
 {
     int a[] = {1, 2, 3, 4, 5};
-    if (isPresent(a, 5, 10))
+    if (isPresent2(a, 5, 13, 0))
     {
         cout << "Found";
     }
