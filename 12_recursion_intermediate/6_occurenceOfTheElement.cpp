848a7e970c0e76eeb62056822ccf6d9247a49b0e
Amit Patil <amitgpatil215@gmail.com>
2021-06-20 14:16:53 +0530

    count 2

--- 12_recursion_intermediate/6_occurenceOfTheElement.cpp
+++ 12_recursion_intermediate/6_occurenceOfTheElement.cpp
@@ -18,10 +18,27 @@ void occurenceOfTheElement(int a[], int n, int x, int i, int &ans)
     occurenceOfTheElement(a, n, x, i + 1, ans);
 }
 
+int occurenceOfTheElement2(int a[], int n, int x, int i)
+{
+
+    //base case
+    if (i == n)
+    {
+        return 0;
+    }
+
+    int howManyTimesPresent = occurenceOfTheElement2(a, n, x, i + 1);
+
+    if (a[i] == x)
+    {
+        howManyTimesPresent = howManyTimesPresent + 1;
+    }
+    return howManyTimesPresent;
+}
+
 int main()
 {
     int a[] = {1, 3, 2, 2, 5, 3, 4, 2};
     int ans = 0;
-    occurenceOfTheElement(a, 8, 2, 0, ans);
-    cout << ans << endl;
+    cout << occurenceOfTheElement2(a, 8, 2, 0);
 }
