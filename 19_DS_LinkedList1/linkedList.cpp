9312b0200d4305871ad6419dbabbb7ca57319afb
Amit Patil <amitgpatil215@gmail.com>
2021-06-27 12:34:26 +0530

    print linked list

---
+++ 19_DS_LinkedList1/linkedList.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    //* dynamically
+    /*
+    Node *n3 = new Node(3);
+    Node *n4 = new Node(4);
+    n3->next = n4;
+    return 0;
+    */
+
+    // statically
+    Node N1(1);
+    Node N2(2);
+    Node N3(3);
+    Node N4(4);
+    Node N5(5);
+
+    Node *head = &N1;
+    N1.next = &N2;
+    N2.next = &N3;
+    N3.next = &N4;
+    N4.next = &N5;
+
+    print(head);
+
+}
