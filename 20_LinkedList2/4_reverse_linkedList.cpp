4bcb74d08cff988ddaadb6ca045f490f84616cc0
Amit Patil <amitgpatil215@gmail.com>
2021-07-01 09:39:25 +0530

    reverse a linked list

---
+++ 20_LinkedList2/4_reverse_linkedList.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+/// inserting at head
+Node *takeInput() /// O(n)
+{
+    int data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    cin >> data;
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        // we have alredy some nodes in linked list
+        if (head != NULL)
+        {
+            tail->next = n;
+            tail = tail->next;
+        }
+        else
+        {
+            head = n;
+            tail = n;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+}
+
+Node *reverseLinkedList(Node *head)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+        Node *n = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = n;
+    }
+    return prev;
+}
+
+int main()
+{
+    Node *head = takeInput();
+    print(reverseLinkedList(head));
+
+    return 0;
+}
