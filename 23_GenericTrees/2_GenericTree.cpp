bce8b6402ccc05543b2e5532426df20f9234ab29
Amit Patil <amitgpatil215@gmail.com>
2021-07-06 14:44:33 +0530

    print tree

--- 23_GenericTrees/2_GenericTree.cpp
+++ 23_GenericTrees/2_GenericTree.cpp
@@ -15,6 +15,25 @@ public:
     }
 };
 
+void printTree(TreeNode<int> *root)
+{
+    //edge case not a base case
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ": ";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << " , ";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+
 int main()
 {
     TreeNode<int> *root = new TreeNode<int>(1);
@@ -23,6 +42,6 @@ int main()
 
     root->children.push_back(n1);
     root->children.push_back(n2);
-
+    printTree(root);
     return 0;
 }
