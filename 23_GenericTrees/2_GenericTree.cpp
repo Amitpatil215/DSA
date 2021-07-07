b09d91d43a605d39565609d6f5d7092497b0a85a
Amit Patil <amitgpatil215@gmail.com>
2021-07-07 09:55:37 +0530

    level order print

--- 23_GenericTrees/2_GenericTree.cpp
+++ 23_GenericTrees/2_GenericTree.cpp
@@ -86,11 +86,31 @@ TreeNode<int> *takeInputLevelWise()
         }
     }
 }
+void levelOrderPrint(TreeNode<int> *root)
+{
+    queue<TreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        TreeNode<int> *front = q.front();
+        q.pop();
+        cout << front->data << " : ";
 
+        for (int i = 0; i < front->children.size(); i++)
+        {
+            cout << root->children[i]->data << " , ";
+        }
+        cout << endl;
+        for (int i = 0; i < front->children.size(); i++)
+        {
+            q.push(front->children[i]);
+        }
+    }
+}
 int main()
 {
     //TreeNode<int> *root = takeInputUsingRecursion();
     TreeNode<int> *root = takeInputLevelWise();
-    printTree(root);
+    levelOrderPrint(root);
     return 0;
 }
