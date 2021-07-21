01c14b72e9cd31db4ec183d33421d67dbb00ca7f
Amit Patil <amitgpatil215@gmail.com>
2021-07-21 14:40:11 +0530

    construct tree from post and in order tree

---
+++ 24_BinaryTree/4_constructFromPostAndInOrderTraversal.cpp
@@ -0,0 +1,53 @@
+#include "1_BinaryTree.cpp"
+BTNode<int> *buildTreeHelper(vector<int> inorder, vector<int> postOrder, int inOrderStart, int inOrderEnd, int postOrderStart, int postOrderEnd)
+{
+    //base case
+    if (inOrderStart > inOrderEnd)
+    {
+        return NULL;
+    }
+
+    int rootData = postOrder[postOrderEnd];
+
+    int rootIndex = -1;
+
+    for (int i = inOrderStart; i <= inOrderEnd; i++)
+    {
+        if (inorder[i] == rootData)
+        {
+            rootIndex = i;
+            break;
+        }
+    }
+
+    int leftInOrderStart = inOrderStart;
+    int leftInOrderEnd = rootIndex - 1;
+    int leftpostOrderStart = postOrderStart;
+    int leftpostOrderEnd = leftpostOrderStart + leftInOrderEnd - leftInOrderStart;
+
+    int rightInOrderStart = rootIndex + 1;
+    int rightInOrderEnd = inOrderEnd;
+    int rightpostOrderStart = leftpostOrderEnd + 1;
+    int rightpostOrderEnd = postOrderEnd - 1;
+
+    BTNode<int> *root = new BTNode<int>(rootData);
+    // build left sub tree
+    root->left = buildTreeHelper(inorder, postOrder, leftInOrderStart, leftInOrderEnd, leftpostOrderStart, leftpostOrderEnd);
+    // build right sub tree
+    root->right = buildTreeHelper(inorder, postOrder, rightInOrderStart, rightInOrderEnd, rightpostOrderStart, rightpostOrderEnd);
+
+    return root;
+}
+
+BTNode<int> *buildTree(vector<int> &postOrder, vector<int> &inorder)
+{
+    int size = inorder.size();
+    return buildTreeHelper(inorder, postOrder, 0, size - 1, 0, size - 1);
+}
+int main()
+{
+    vector<int> postOrder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+    BTNode<int> *root = buildTree(postOrder, inorder);
+    InOrderTrraversal(root);
+}
