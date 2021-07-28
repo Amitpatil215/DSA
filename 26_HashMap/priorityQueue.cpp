bbe997ba667718d8f1253ace84b59583487ed106
Amit Patil <amitgpatil215@gmail.com>
2021-07-28 12:45:15 +0530

    priority queue custom implementaion

--- 26_HashMap/priorityQueue.cpp
+++ 26_HashMap/priorityQueue.cpp
@@ -46,9 +46,63 @@ public:
             }
         }
     }
+    int removeMin()
+    {
+        if (isEmpty())
+            return 0;
+
+        int lastElementIndex = pq.size() - 1;
+        int removedElement = pq[0];
+        swap(pq[0], pq[lastElementIndex]);
+        pq.pop_back();
+
+        //check for min Heap properties
+        int parentIndex = 0;
+        while (true)
+        {
+            int leftChildIndex = 2 * parentIndex + 1;
+            int rightChildIndex = 2 * parentIndex + 2;
+            int minElmentIndex = parentIndex;
+            if (leftChildIndex < pq.size() && pq[leftChildIndex] < pq[minElmentIndex])
+            {
+                minElmentIndex = leftChildIndex;
+            }
+            if (rightChildIndex < pq.size() && pq[rightChildIndex] < pq[minElmentIndex])
+            {
+                minElmentIndex = rightChildIndex;
+            }
+            if (minElmentIndex == parentIndex)
+                break;
+            swap(pq[parentIndex], pq[minElmentIndex]);
+            parentIndex = minElmentIndex;
+        }
+        return removedElement;
+    }
 };
 int main()
 {
-
+    PriorityQueue pq;
+    pq.insert(100);
+    pq.insert(10);
+    pq.insert(15);
+    pq.insert(4);
+    pq.insert(17);
+    pq.insert(21);
+    pq.insert(67);
+    cout << pq.getSize() << endl;
+    cout << pq.getMin() << endl;
+    while (!pq.isEmpty())
+    {
+        cout << pq.removeMin() << endl;
+    }
+    /* 7
+       4
+       4
+       10
+       15
+       1721
+       67
+       100
+       */
     return 0;
 }
