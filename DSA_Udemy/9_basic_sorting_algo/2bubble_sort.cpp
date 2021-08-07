800f2f7f42b0329dd920e5f84e8eea20f367b65f
Amit Patil <amitgpatil215@gmail.com>
2021-08-07 11:51:53 +0530

    reformat

--- DSA_Udemy/9_basic_sorting_algo/2bubble_sort.cpp
+++ DSA_Udemy/9_basic_sorting_algo/2bubble_sort.cpp
@@ -4,10 +4,13 @@ using namespace std;
 void bubbleSort(int a[], int n)
 {
 
-    for(int count=0;count<n-1;count++){
-        for(int j=0;j<n-1;j++){
-            if(a[j]>a[j+1]){
-                swap(a[j],a[j+1]);
+    for (int count = 0; count < n - 1; count++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                swap(a[j], a[j + 1]);
             }
         }
     }
@@ -20,13 +23,14 @@ int main()
     int a[n];
     for (int i = 0; i < n; i++)
     {
-        cin>> a[i];
+        cin >> a[i];
     }
 
-    bubbleSort(a,n);
+    bubbleSort(a, n);
 
-    for(int i=0;i<n;i++){
-        cout<<a[i]<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << endl;
     }
 
     return 0;
