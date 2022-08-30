a86dfe3a1f4b1af68bcbeb8438a52a1392208c63
Amit Patil <amitgpatil215@gmail.com>
2022-08-30 16:29:44 +0530

    segemnt tree query and build

--- 10_competative/Range_queries.cpp/6_segmet_tree_build_query.cpp
+++ 10_competative/Range_queries.cpp/6_segmet_tree_build_query.cpp
@@ -1,3 +1,58 @@
+/*
+Max In An Interval - Range Query
+Medium  Prev   Next
+You are given an array(of integers) of length n.
+You are required to answer q queries.
+In each query u are given an interval l, r both inclusive and u have to find the
+maximum element in this interval.
+
+To do the above task u have to create a datastructure as follows :-
+
+Implement the SegmentTree class:
+1. SegmentTree(int arr[]): Initializes the SegmentTree object with an array,
+2. int query(int l, int r): return max in interval [l, r].
+
+Can u do it in O(log(n)) or better Time Complexity.
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+A number q
+following q lines contains queries of format
+l r
+Output Format
+for each query print a single integer in seperate line
+Question Video
+
+  COMMENTConstraints
+1. 1 <= n, q <= 10^5
+2. 0 <= l <= r < n
+3. 10^9 <= arr[i] <= 10^9
+4. all input and output will fit in 32bit signed integer
+Sample Input
+8
+8
+7
+4
+2
+5
+3
+1
+10
+4
+0 7
+0 3
+2 7
+1 6
+Sample Output
+10
+8
+10
+7
+
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
