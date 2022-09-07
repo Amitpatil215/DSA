20d18ec4de400332e8efbfcdd5e70cfdd1f71269
Amit Patil <amitgpatil215@gmail.com>
2022-09-08 03:12:11 +0530

    make array elemnts equal

--- playground/main.cpp
+++ playground/main.cpp
@@ -15,79 +15,46 @@ void __f(const char *names, Arg1 &&arg1, Args &&...args) {
     __f(comma + 1, args...);
 }
 
-class segmentTree {
-   public:
-    // given array
-    vector<int> arr;
-
-    // to store segment tree
-    vector<int> tree;
-    // constructor
-    segmentTree(int n, vector<int> arr) {
-        this->arr = arr;
-        tree = vector<int>(4 * n);
-        // build the tree using given array
-        buildTree(1, 0, n - 1);
-        for (int i = 0; i < tree.size(); i++) {
-            debug(i, tree[i]);
+bool isPossibleToDivide(int targetSum, vector<int> &v) {
+    int tempSum = 0;
+    for (auto a : v) {
+        tempSum += a;
+        if (tempSum == targetSum) {
+            tempSum = 0;
+        } else if (tempSum > targetSum) {
+            return false;
         }
     }
-    void buildTree(int node, int start, int end) {
-        // if we are left eith only one element the that would be max;
-        if (start == end) {
-            tree[node] = arr[start];
-            return;
-        }
-        int mid = (start + end) / 2;
-        int left = node * 2;
-        int right = node * 2 + 1;
-        // build left and right sub tree
-        buildTree(left, start, mid);
-        buildTree(right, mid + 1, end);
-        // store the max value from left and right tree
-        tree[node] = max(tree[left], tree[right]);
-    }
-
-    // start and end denotes current nodes range
-    // l and r denotes left and right range in the query
-    int query(int node, int start, int end, int l, int r) {
-        // when given l and r are out of range
-        if (r < start || l > end) return INT_MIN;
-        // if start and end is same
-        if (start == end) {
-            return tree[node];
-        } else if (l <= start && end <= r) {
-            // if l and r contains the range i.e start and end
-            return tree[node];
-        } else {
-            int mid = (start + end) / 2;
-            int left = query(node * 2, start, mid, l, r);
-            int right = query((node * 2) + 1, mid + 1, end, l, r);
-            return max(left, right);
-        };
-    }
-};
+    return true;
+}
 
 void solve() {
-    int n, q;
+    int n;
     cin >> n;
-    vector<int> arr(n);
+    vector<int> v(n);
+    int arraySum = 0;
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> v[i];
+        arraySum += v[i];
     }
-    cin >> q;
-    vector<pair<int, int>> query;
-    for (int i = 0; i < q; i++) {
-        int l, r;
-        cin >> l >> r;
-        query.push_back({l, r});
+
+    // we can devide array to at max n parts
+    // check is it possible to divide i no of segments with equal sum
+    // if possible then go in array and check does that sum actaull possible or
+    // not
+
+    // maxLength possible for now is 1 as we can combine all elements and get
+    // one elemnt at the ends
+    int maxLength = 1;
+    for (int i = 1; i <= n; i++) {
+        if (arraySum % i == 0 && isPossibleToDivide(arraySum / i, v)) {
+            maxLength = i;
+        }
     }
-    segmentTree segTree(n, arr);
-    for (auto qu : query) {
-        int l = qu.first;
-        int r = qu.second;
-        cout << segTree.query(1, 0, arr.size() - 1, l, r) << endl;
-    }
+
+    cout << "length of final array " << maxLength << endl;
+    cout << "no of operations req " << n - maxLength << endl;
+    cout<<endl;
 }
 
 int main() {
@@ -104,63 +71,3 @@ int main() {
     cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
     return 0;
 }
-
-/*
-Test case
-1 // no of test cases
-8 // total numbers in array
-8
-7
-4
-2
-5
-3
-1
-10
-4 // total no fo queries
-0 7
-0 3
-2 7
-1 6
-
-Output
-
-i : 0 |  tree[i] : 0
-i : 1 |  tree[i] : 10
-i : 2 |  tree[i] : 8
-i : 3 |  tree[i] : 10
-i : 4 |  tree[i] : 8
-i : 5 |  tree[i] : 4
-i : 6 |  tree[i] : 5
-i : 7 |  tree[i] : 10
-i : 8 |  tree[i] : 8
-i : 9 |  tree[i] : 7
-i : 10 |  tree[i] : 4
-i : 11 |  tree[i] : 2
-i : 12 |  tree[i] : 5
-i : 13 |  tree[i] : 3
-i : 14 |  tree[i] : 1
-i : 15 |  tree[i] : 10
-i : 16 |  tree[i] : 0
-i : 17 |  tree[i] : 0
-i : 18 |  tree[i] : 0
-i : 19 |  tree[i] : 0
-i : 20 |  tree[i] : 0
-i : 21 |  tree[i] : 0
-i : 22 |  tree[i] : 0
-i : 23 |  tree[i] : 0
-i : 24 |  tree[i] : 0
-i : 25 |  tree[i] : 0
-i : 26 |  tree[i] : 0
-i : 27 |  tree[i] : 0
-i : 28 |  tree[i] : 0
-i : 29 |  tree[i] : 0
-i : 30 |  tree[i] : 0
-i : 31 |  tree[i] : 0
-10
-8
-10
-7
-
-
- */
